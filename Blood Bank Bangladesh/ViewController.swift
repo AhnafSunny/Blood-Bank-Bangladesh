b82ff4fbd296d22f3f7645dc79aaab00bcc1c727
ahnafsunny <ahnafx88@gmail.com>
2017-04-20 20:54:10 +0600

    Initial Commit

---
+++ Blood Bank Bangladesh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blood Bank Bangladesh
+//
+//  Created by Ahnaf on 4/20/17.
+//  Copyright © 2017 Ahnaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
