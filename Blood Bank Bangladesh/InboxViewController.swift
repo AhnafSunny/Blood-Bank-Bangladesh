1fcb2f59603f21db727bc30a4953d4331a064b73
ahnafsunny <ahnafx88@gmail.com>
2017-05-03 23:43:08 +0600

    Message list Received and populated in tableview

--- Blood Bank Bangladesh/InboxViewController.swift
+++ Blood Bank Bangladesh/InboxViewController.swift
@@ -2,37 +2,98 @@
 //  InboxViewController.swift
 //  Blood Bank Bangladesh
 //
-//  Created by Ahnaf on 4/30/17.
+//  Created by Ahnaf on 5/3/17.
 //  Copyright © 2017 Ahnaf. All rights reserved.
 //
 
 import UIKit
+import FirebaseDatabase
 import FirebaseAuth
 
-class InboxViewController: UIViewController {
+class InboxViewController: UITableViewController {
+    var index = 0
+    var messages: [FIRDataSnapshot]! = [FIRDataSnapshot]()
+    
+    var ref: FIRDatabaseReference?
+    var databaseHandle: FIRDatabaseHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        FetchData()
         
         
-
-        // Do any additional setup after loading the view.
+        
     }
+    
+    deinit{
+        self.ref?.child("messages").removeObserver(withHandle: databaseHandle!)
+        
+    }
+    
+    
+    func FetchData(){
+        //get current user email
+        let currEmail = FIRAuth.auth()?.currentUser?.email!
+        //print(currEmail)
+        let newemail = Utilities().createNewEmail(oldEmail: currEmail!)
+        print("inside fetch data")
+        
+        
+        ref = FIRDatabase.database().reference()
+        databaseHandle = self.ref?.child("Messages").child(newemail).observe(.childAdded,with: {
+            (snapshot) -> Void in
+            self.messages.append(snapshot)
+            self.tableView.insertRows(at: [IndexPath(row: self.messages.count-1,section:0)], with: .automatic)
+            
+        })
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return messages.count
+    }
     
 
-    /*
-    // MARK: - Navigation
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        
+        let messageSnap: FIRDataSnapshot! = self.messages[indexPath.row]
+        let message = messageSnap.value as! Dictionary<String , String>
+        if let text = message[Constants.RequestMessage.sender] as String! {
+            cell.textLabel?.text = text
+        }
+        
+        if let subText = message[Constants.RequestMessage.message]{
+            cell.detailTextLabel?.text = subText
+            
+        }
+        
+        
+        return cell
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        index = indexPath.row
+        performSegue(withIdentifier: "mdSegue", sender: self)
+    }
+    
 }
